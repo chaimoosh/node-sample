eaee913c2ffe0a45b8afd2ea15e9c4b69551310f
Chaim Reisman <chaimoosh@gmail.com>
2018-10-15 12:41:37 -0400

    added more

--- blog/app.js
+++ blog/app.js
@@ -1,41 +1,83 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const logger = require("morgan");
+const path = require("path");
+const okta = require("@okta/okta-sdk-nodejs");
+const session = require("express-session");
+const ExpressOIDC = require("@okta/oidc-middleware").ExpressOIDC;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const blogRouter = require("./routes/blog");
+const usersRouter = require("./routes/users");
 
-var app = express();
+const app = express();
+const client = new okta.Client({
+  orgUrl: "{https://dev-703184.oktapreview.com}",
+  token: "{00ub48YotZDJ9-Mx8XGCnQYuLwd1UO16BnFIHPD-bW}"
+});
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+// Middleware
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const oidc = new ExpressOIDC({
+  issuer: "{https://dev-703184.oktapreview.com}/oauth2/default",
+  client_id: "{0oagnbx3oyVeGsH6E0h7}",
+  client_secret: "{XgFP8gYKpfrluiLmQ26G6XslsP_41_x4jcO-3Epq}",
+  redirect_uri: "http://localhost:3000/users/callback",
+  scope: "openid profile",
+  routes: {
+    login: {
+      path: "/users/login"
+    },
+    callback: {
+      path: "/users/callback",
+      defaultRedirect: "/dashboard"
+    }
+  }
+});
 
-// catch 404 and forward to error handler
+app.use(session({
+  secret: "{sjfghsuyghkjvhfsljhgsriuhgreiudh}",
+  resave: true,
+  saveUninitialized: false
+}));
+
+app.use(oidc.router);
+
+app.use((req, res, next) => {
+  if (!req.userinfo) {
+    return next();
+  }
+
+  client.getUser(req.userinfo.sub)
+    .then(user => {
+      req.user = user;
+      res.locals.user = user;
+      next();
+    });
+});
+
+// Routes
+app.use("/", blogRouter);
+app.use("/users", usersRouter);
+
+// Error handlers
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
+
 
 module.exports = app;
